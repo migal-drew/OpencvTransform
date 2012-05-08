15a69f4f93efa808664918a0ad57a4eb9245f9f0
andrew <andrew@andrew-VirtualBox.(none)>
2012-05-08 16:10:16 +0400

    added mosaic

--- src/mosaic_cool.py
+++ src/mosaic_cool.py
@@ -27,20 +27,39 @@ def transformPoint(p, theta):
     ans = np.dot(rot, ans)
     #Translation
     ans = ans - trans
+    print ans
     return ans
 
-def costFunction(points_1, points_2, theta_1, theta_2):
+def costFunction(points_1, points_2, theta_1, theta_2, lambd):
+    m = points_1.shape[0]
+    J = 0;
+    for i in range(m):
+        error = np.sum(np.square( transformPoint(points_1[i], theta_1) -
+            transformPoint(points_2[i], theta_2) ))
+        J = J + error
+        print error
     
-    return None
+    regularize = (lambd / (2 * m)) * (np.sum((np.square(theta_1[1:5]) +
+                                      np.square(theta_2[1:5]))) - 4)
+                                      #s_x, s_y normally = 1 => sum = 4
+    #regularize = regularize - 4 
+    J = J / (2 * m) + regularize
+    return J
 
 if __name__ == '__main__':
-    theta = np.array([0, 2, 2, 0, 0, 0, 0])
+    theta_1 = np.array([0, 1, 1, 0, 0, 0, 0])
+    theta_2 = np.array([0, 1, 1, 0, 0, 0, 0])
     p1 = np.array( [50, 150] )
     p2 = np.array( [150, 150] )
     p3 = np.array( [150, 50] )
     p4 = np.array( [50, 50] )
-    print transformPoint(p1, theta)
-    print transformPoint(p2, theta)
-    print transformPoint(p3, theta)
-    print transformPoint(p4, theta)
+    #print p1.shape
+    #print transformPoint(p1, theta)
+    #print transformPoint(p2, theta)
+    #print transformPoint(p3, theta)
+    #print transformPoint(p4, theta)
+    
+    po_1 = po_2 = np.array( [p1, p2, p3, p4] )
+    #print po_1
+    print costFunction(po_1, po_2, theta_1, theta_2, 0.1)
     
